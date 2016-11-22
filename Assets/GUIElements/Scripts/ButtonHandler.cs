e9ed7a70134fed51e471906ba54d75f1312f009a
Glänn <hallda@utb.fuv.se>
2016-11-22 15:59:16 +0100

    Fixed a profile icon for interaction. - added a "Create profile" button - Icons give feedback

---
+++ Assets/GUIElements/Scripts/ButtonHandler.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour {
+    private bool isVisible = false;
+    private bool insideProfile = false;
+    private bool insideTrash = false;
+    private bool insidePlay = false;
+    [SerializeField] private GameObject trashBin;
+    [SerializeField] private GameObject playButton;
+
+    public bool IsVisible
+    {
+        get { return isVisible; }
+        set { isVisible = value; }
+    }
+
+    public bool InsideProfile
+    {
+        get { return insideProfile; }
+        set { insideProfile = value; }
+    }
+
+    public bool InsideTrash
+    {
+        get { return insideTrash; }
+        set { insideTrash = value; }
+    }
+
+    public bool InsidePlay
+    {
+        get { return insidePlay; }
+        set { insidePlay = value; }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        trashBin = GameObject.FindGameObjectWithTag("TrashCan");
+        playButton = GameObject.FindGameObjectWithTag("PlayButton");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(InsideProfile || InsideTrash || InsidePlay)
+        {
+            isVisible = true;
+        }
+	    if(!InsideProfile && !InsideTrash && !InsidePlay)
+        {
+            isVisible = false;
+        }
+
+        if(isVisible)
+        {
+            //TODO: find the trashcan and the play button and add them to the script
+            trashBin.SetActive(true);
+            playButton.SetActive(true);
+        }
+
+        else
+        {
+            trashBin.SetActive(false);
+            playButton.SetActive(false);
+        }
+	}
+}
---
+++ Assets/GUIElements/Scripts/UIHandler.cs
@@ -0,0 +1,63 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.UI;
+
+//[RequireComponent(typeof(Renderer))]
+//public class UIHandler : MonoBehaviour
+//{
+
+//    [SerializeField] private GameObject trashCan;
+//    [SerializeField] private GameObject playButton;
+
+//    [SerializeField] private bool isVisible = false;
+
+//    public Renderer rend;
+
+//    void Start()
+//    {
+//        rend = GetComponent<Renderer>();
+//        //Debug.Log("Inside interaction area");
+//        playButton = GameObject.FindGameObjectWithTag("PlayButton");
+//        trashCan = GameObject.FindGameObjectWithTag("TrashCan");
+//    }
+
+//    void OnMouseEnter()
+//    {
+//        Debug.Log("Mouse is over!");
+//        isVisible = false;
+//    }
+
+//    //void FixedUpdate()
+//    //{
+//    //    if(isVisible)
+//    //    {
+//    //        Debug.Log("Is active");
+//    //        trashCan.SetActive(true);
+//    //        playButton.SetActive(true);
+             
+//    //    }
+
+//    //    else
+//    //    {
+//    //        //Debug.Log("Is not active");
+//    //        trashCan.SetActive(false);
+//    //        playButton.SetActive(false);
+//    //    }
+//    //}
+
+//    //void OnMouseExit()
+//    //{
+//    //    isVisible = false;
+//    //}
+
+
+
+
+
+
+
+
+
+
+
+//}
