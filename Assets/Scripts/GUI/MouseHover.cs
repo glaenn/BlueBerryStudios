269404db657a032b2cb4033ce7760f7a0cf42288
Glänn <hallda@utb.fuv.se>
2016-11-23 13:01:14 +0100

    General fix from previous scene.
    * Reshaped the pofile screen to fit to changes in the aspect ratio.
    * Added a vectorised text for header instead of using inside UI text block
    * Changed the ways of finding correct hidden object via an array instead of using tags
    * Tags "PlayerButton" "TrashBin" "InteractionLayout" removed from Tag Manager
    * Added smoother hover effects on play and delete button.

---
+++ Assets/Scripts/GUI/MouseHover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class MouseHover : MonoBehaviour
+{
+    [SerializeField] private GameObject[] hiddenGameObject;    
+
+    void Start()
+    {
+        SetVisibile(false);
+    }
+
+    public void SetVisibile(bool isVisible)
+    {
+        Debug.Log("DERP!");
+        foreach (GameObject hiddenObject in hiddenGameObject)
+        {
+            hiddenObject.SetActive(isVisible);
+        }
+
+    }
+
+   
+
+
+}
