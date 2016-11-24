a4555936cb1283d99e57d16b9b28797ff3ae0ae5
Glänn <hallda@utb.fuv.se>
2016-11-24 10:45:10 +0100

    Started the Profile Manager script to handle all information about the user

---
+++ Assets/Scripts/GameController/ProfileManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProfileManager : MonoBehaviour
+{
+    struct ProfileData
+    {
+        public string name;
+        public float playerMinutes;
+        public Color playerColor;
+
+        public string GetName()
+        {
+            return name;
+
+        }
+
+        public void CreateNewData(string name)
+        {
+
+        }
+    }
+
+    List<ProfileData> profileData = new List<ProfileData>();
+
+    public void AddProfile()
+    {
+        profileData.Add(new ProfileData());            
+    }
+
+    public void RemoveProfile()
+    {
+        profileData.RemoveAt(2);
+    }
+
+    public void GetProfile(int i)
+    {
+        ProfileData newProfile = profileData[i];
+
+    }
+
+
+	
+}
