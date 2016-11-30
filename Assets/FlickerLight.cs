d24023bf4dd7cac5c10f085a018b28eeccdd4092
Glänn <hallda@utb.fuv.se>
2016-11-30 12:38:44 +0100

    Working on main menu effects using particles and shapes

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerLight : MonoBehaviour
+{
+    //Do not use 
+
+    [SerializeField] private float minFlickerIntensity = 0.5f;
+    [SerializeField] private float maxFlickerIntensity = 1f;
+    [SerializeField] private float flickerSpeed = 0.035f;
+    [SerializeField] private float timer = 2f;
+
+
+    [SerializeField]
+    ParticleSystem.ShapeModule particleShape;    
+
+    void Start()
+    {
+        particleShape = GetComponent<ParticleSystem.ShapeModule>();
+    }
+
+    void Update()
+    {
+        if (timer > flickerSpeed)
+        {
+            particleShape.radius = Random.Range(minFlickerIntensity, maxFlickerIntensity);
+            timer = 0;
+        }
+
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    
+
+
+
+}
